bb6fad0fd0cc7cce0e47d7923826d51c1df0f14d
Yonatan Addis <yonatanaddis0257@gmail.com>
2022-11-04 19:04:26 +0300

    Bug fix

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -41,3 +41,4 @@ void bubble_sort(int *array, size_t size)
 		}
 	}
 }
+
--- sort.h
+++ sort.h
@@ -19,5 +19,7 @@ typedef struct listint_s
 void bubble_sort(int *array, size_t size);
 void print_array(const int *array, size_t size);
 void print_list(const listint_t *list);
+void insertion_sort_list(listint_t **list);
 
 #endif /*SORT_H*/
+
