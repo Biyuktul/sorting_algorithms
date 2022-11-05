0eb6bc9417122630c672d9f20594e54758a62203
Yonatan Addis <yonatanaddis0257@gmail.com>
2022-11-05 07:04:06 -0700

    Updating

--- print_array.c
+++ print_array.c
@@ -14,8 +14,8 @@ void print_array(const int *array, size_t size)
 	i = 0;
 	while (array && i < size)
 	{
-		if (i > 0)
-			printf(", ");
+	if (i > 0)
+		printf(", ");
 		printf("%d", array[i]);
 		++i;
 	}
