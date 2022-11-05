f582b55ddd33a4c08a58830879cbcc4cbb16d2ac
Yonatan Addis <yonatanaddis0257@gmail.com>
2022-11-05 16:39:44 +0300

    Adding files

---
+++ 1-insertion_sort_list.c
@@ -0,0 +1,51 @@
+#include "sort.h"
+
+/**
+ * swap - swap two consecutive nodes
+ * @node: pointer to the node tobe swaped
+ * @head: double pointer to start of the list
+ * Return: pointer to the first of two swaped nodes
+ */
+
+listint_t *swap(listint_t *node, listint_t **head)
+{
+	listint_t *back = node->prev;
+	listint_t *current = node;
+
+	back->next = current->next;
+	if (current->next != NULL)
+		current->next->prev = back;
+
+	current->next = back;
+	current->prev = back->prev;
+	back->prev = current;
+	if (current->prev != NULL)
+		current->prev->next = current;
+	else
+		*head = current;
+	return (current);
+}
+
+/**
+ * insertion_sort_list - sorts a doubly linked-list using insertion sorted
+ * @list: pointer to the start of the list tobe sorted
+ * Return: void
+*/
+
+void insertion_sort_list(listint_t **list)
+{
+	listint_t *curr = (*list)->next;
+
+	if (curr == NULL)
+		return;
+
+	while (curr)
+	{
+		while ((curr->prev != NULL) && (curr->prev->n > curr->n))
+		{
+			curr = swap(curr, list);
+			print_list(*list);
+		}
+		curr = curr->next;
+	}
+}
--- sort.h
+++ sort.h
@@ -1,6 +1,7 @@
 #ifndef SORT_H
 #define SORT_H
 #include <stdlib.h>
+#include <stdio.h>
 
 /**
  * struct listint_s - Doubly linked list node
@@ -16,10 +17,11 @@ typedef struct listint_s
 	struct listint_s *next;
 } listint_t;
 
-void bubble_sort(int *array, size_t size);
 void print_array(const int *array, size_t size);
 void print_list(const listint_t *list);
+void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
+listint_t *swap(listint_t *node, listint_t **head);
 
 #endif /*SORT_H*/
 
