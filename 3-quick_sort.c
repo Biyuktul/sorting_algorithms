ae906b80399b31734aa0b3db2af3ebe5dc77e086
Yonatan Addis <yonatanaddis0257@gmail.com>
2022-11-07 16:27:50 +0300

    Adding file

---
+++ 3-quick_sort.c
@@ -0,0 +1,87 @@
+#include "sort.h"
+
+/**
+ * swapI - swaps two ints speified by idx1 and idx2
+ * @array: the array whose element is going tobe swapped
+ * @idx1: the first index
+ * @idx2: the second index
+ * Return: Always void
+*/
+
+void swapI(int *array, size_t idx1, size_t idx2)
+{
+	int temp;
+
+	temp = array[idx1];
+	array[idx1] = array[idx2];
+	array[idx2] = temp;
+}
+
+/**
+ * quick_sort - sort an array using quick sort algorithm
+ * @array: the array tobe sorted
+ * @size: the size of the array
+ * Return: always void
+*/
+
+void quick_sort(int *array, size_t size)
+{
+	quickSortRecursion(array, 0, size - 1, size);
+}
+
+/**
+ * quickSortRecursion - Recursively calls it's self for
+ *      different partiotions of the array
+ * @array: the array to be sorted
+ * @low: start index of the array
+ * @high: end index of the array
+ * @size: size of the array
+ * Return: Always void
+*/
+
+void quickSortRecursion(int *array, int low, int high, int size)
+{
+	if (low < high)
+	{
+		int pivot_index = lomuto_partition(array, low, high, size);
+		/*for left partition*/
+		quickSortRecursion(array, low, pivot_index - 1, size);
+		/*for right partition*/
+		quickSortRecursion(array, pivot_index + 1, high, size);
+	}
+}
+
+/**
+ * lomuto_partition - partitions an array around pivot element
+ * @array: array to be partitiond
+ * @low: start index
+ * @high: end index
+ * @size: size of the array
+ * Return: the new pivot index
+*/
+
+int lomuto_partition(int *array, int low, int high, int size)
+{
+	int i = low;
+	int j;
+	int pivot = array[high];
+
+	for (j = low; j < high; j++)
+	{
+		if (array[j] < pivot)
+		{
+			if (array[i] != array[j])
+			{
+				swapI(array, i, j);
+				print_array(array, size);
+			}
+			i++;
+		}
+	}
+	if (array[i] != array[high])
+	{
+		swapI(array, i, high);
+		print_array(array, size);
+	}
+	return (i);
+}
--- sort.h
+++ sort.h
@@ -22,8 +22,11 @@ void print_list(const listint_t *list);
 void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
 void swapInt(int *a, int *b);
+void swapI(int *array, size_t idx1, size_t idx2);
 void selection_sort(int *array, size_t size);
 listint_t *swap(listint_t *node, listint_t **head);
+void quick_sort(int *array, size_t size);
+void quickSortRecursion(int *array, int low, int high, int size);
+int lomuto_partition(int *array, int low, int high, int size);
 
 #endif /*SORT_H*/
-
