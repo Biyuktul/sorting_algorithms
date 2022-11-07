23e3ab6d73ffe1163f682bf1912922f26593b78d
Yonatan Addis <yonatanaddis0257@gmail.com>
2022-11-07 16:33:20 +0300

    betty fix

--- sort.h
+++ sort.h
@@ -28,5 +28,4 @@ listint_t *swap(listint_t *node, listint_t **head);
 void quick_sort(int *array, size_t size);
 void quickSortRecursion(int *array, int low, int high, int size);
 int lomuto_partition(int *array, int low, int high, int size);
-
 #endif /*SORT_H*/
