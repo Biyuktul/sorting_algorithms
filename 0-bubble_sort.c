5cc8971bfcfb10bd33cbbd7f9a2da238b730ac05
adam <adam@.gmail.com>
2023-07-21 06:08:52 -0700

    0-tasks: Make 0-bubble_sort() more efficient

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -12,9 +12,11 @@ void bubble_sort(int *array, size_t size)
 {
 	size_t i, j;
 
-	for (i = 0; i < size; i++)
+	/* For more efficiency i loops from 0 to size - 1*/
+	for (i = 0; i < size - 1; i++)
 	{
-		for (j = 0; j < size - 1; j++)
+		/* And j loops from 0 to size - 1 - i*/
+		for (j = 0; j < size - 1 - i; j++)
 		{
 			if (array[j] > array[j + 1])
 			{
