f2239d2b6b6aaac4dd1574004e59f08979a8fd91
adam <adam@.gmail.com>
2023-07-24 07:34:19 -0700

    102.task

--- 102-counting_sort.c
+++ 102-counting_sort.c
@@ -13,6 +13,8 @@ void counting_sort(int *array, size_t size)
 	int *count_array;
 	int *temp_array = malloc(sizeof(int) * size);
 
+	if (temp_array == NULL)
+		return;
 	for (i = 0; i < size; i++)
 	{
 		temp_array[i] = array[i];
@@ -22,21 +24,22 @@ void counting_sort(int *array, size_t size)
 			k = array[i];
 	}
 	count_array = malloc(sizeof(int) * (k + 1));
+	if (count_array == NULL)
+	{
+		free(temp_array);
+		return;
+	}
 	for (j = 0; j < k + 1; j++)
 		count_array[j] = 0;
 	for (i = 0; i < size; i++)
-	{
 		count_array[(array[i])] += 1;
-	}
 	for (j = 0; j < k + 1; j++)
 	{
 		if (j == 0)
 			continue;
 		count_array[j] += count_array[j - 1];
 	}
-
 	print_array(count_array, k + 1);
-
 	for (i = 0; i < size; i++)
 	{
 		count_array[temp_array[i]] -= 1;
