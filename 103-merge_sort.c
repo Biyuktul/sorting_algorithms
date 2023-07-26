b175ff375039b201b00ed0abc236c6ac71be5c0f
adam <adam@.gmail.com>
2023-07-25 18:44:40 -0700

    Fixing issues

--- 103-merge_sort.c
+++ 103-merge_sort.c
@@ -4,23 +4,28 @@
 
 void merge_recursion(size_t low, size_t high, int *array);
 
+/**
+  * merge_sort - merge sort algo
+  * @array: array
+  * @size: size
+  */
 void merge_sort(int *array, size_t size)
 {
-	int *s_array = malloc(sizeof(int) * size);
-
-	if (s_array == NULL)
-		return;
-	merge_recursion(0, size - 1, 0, s_array);
+	merge_recursion(0, size - 1, array);
 }
 
-void merge_recursion(size_t low, size_t high, int i, int *s_array)
+/**
+  * merge_recursion - merge recursion func
+  * @low: low
+  * @high: high
+  * @array: array
+  */
+void merge_recursion(size_t low, size_t high, int *array)
 {
 	if (low != high)
 	{
-		merge_recursion(low, (high + low + 1) / 2 - 1, i++, s_array);
-		merge_recursion((high + low + 1) / 2, high, i++,  array);
+		merge_recursion(low, (high + low + 1) / 2 - 1, array);
+		merge_recursion((high + low + 1) / 2, high, array);
 	} else
-	{
-
-	}
+		printf("%d\n", array[low]);
 }
